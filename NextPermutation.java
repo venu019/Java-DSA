1d22c4de51c6923eca9235e6fafae280ec082c63
Chelliboyina Venu Gopal <venugopalchelliboyina@gmail.com>
2025-03-03 22:25:40 +0530

    Create NextPermutation.java

---
+++ NextPermutation.java
@@ -0,0 +1,75 @@
+// 31. Next Permutation
+// A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+
+// For example, for arr = [1,2,3], the following are all the permutations of arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+// The next permutation of an array of integers is the next lexicographically greater permutation of its integer. More formally, if all the permutations of the array are sorted in one container according to their lexicographical order, then the next permutation of that array is the permutation that follows it in the sorted container. If such arrangement is not possible, the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
+
+// For example, the next permutation of arr = [1,2,3] is [1,3,2].
+// Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+// While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
+// Given an array of integers nums, find the next permutation of nums.
+
+// The replacement must be in place and use only constant extra memory.
+
+// Example 1:
+// Input: nums = [1,2,3]
+// Output: [1,3,2]
+
+// Example 2:
+// Input: nums = [3,2,1]
+// Output: [1,2,3]
+
+// Example 3:
+// Input: nums = [1,1,5]
+// Output: [1,5,1]
+ 
+
+// Constraints:
+// 1 <= nums.length <= 100
+// 0 <= nums[i] <= 100
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int ind = -1;
+        
+        // Step 1: Find the first decreasing element from the right
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                ind = i;
+                break;
+            }
+        }
+        
+        // Step 2: If no such element is found, reverse the entire array (last permutation case)
+        if (ind == -1) {
+            reverse(nums, 0, n - 1);
+            return;
+        }
+        
+        // Step 3: Find the next larger element to swap with nums[ind]
+        for (int i = n - 1; i > ind; i--) {
+            if (nums[i] > nums[ind]) {
+                swap(nums, i, ind);
+                break;
+            }
+        }
+        
+        // Step 4: Reverse the suffix to get the next permutation
+        reverse(nums, ind + 1, n - 1);
+    }
+    
+    public void swap(int[] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+    
+    public void reverse(int[] a, int i, int j) {
+        while (i < j) { // Fixed this condition
+            swap(a, i, j);
+            i++;
+            j--;
+        }
+    }
+}
