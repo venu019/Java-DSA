70bc3470b9de5ee163a3007ded1271a545279b53
Chelliboyina Venu Gopal <venugopalchelliboyina@gmail.com>
2025-03-09 20:49:10 +0530

    Create Majority Element II.java

---
+++ Majority Element II.java
@@ -0,0 +1,59 @@
+// 229. Majority Element II
+// Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
+
+// Example 1:
+// Input: nums = [3,2,3]
+// Output: [3]
+
+// Example 2:
+// Input: nums = [1]
+// Output: [1]
+
+// Example 3:
+// Input: nums = [1,2]
+// Output: [1,2]
+
+// Constraints:
+// 1 <= nums.length <= 5 * 104
+// -109 <= nums[i] <= 109
+
+import java.util.*;
+
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int count1 = 0, count2 = 0, candidate1 = 0, candidate2 = 1;
+
+        // Step 1: Find potential candidates using Boyer-Moore Voting Algorithm
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        // Step 2: Verify the candidates
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1) count1++;
+            if (num == candidate2) count2++;
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int n = nums.length;
+        if (count1 > n / 3) result.add(candidate1);
+        if (count2 > n / 3 && candidate1 != candidate2) result.add(candidate2);
+
+        return result;
+    }
+}
