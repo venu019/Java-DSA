ba09077329b5baddead19e9e0ac169bafb00d3bf
Chelliboyina Venu Gopal <venugopalchelliboyina@gmail.com>
2025-03-25 19:04:57 +0530

    Create singleNonDuplicate.java

---
+++ Binary Search/singleNonDuplicate.java
@@ -0,0 +1,39 @@
+// 540. Single Element in a Sorted Array
+// You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
+// Return the single element that appears only once.
+// Your solution must run in O(log n) time and O(1) space.
+
+// Example 1:
+// Input: nums = [1,1,2,3,3,4,4,8,8]
+// Output: 2
+  
+// Example 2:
+// Input: nums = [3,3,7,7,10,11,11]
+// Output: 10
+ 
+// Constraints:
+// 1 <= nums.length <= 105
+// 0 <= nums[i] <= 105
+
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            
+            if (mid % 2 == 1) {
+                mid--; 
+            }
+            
+            if (nums[mid] == nums[mid + 1]) {
+                start = mid + 2;
+            } else {
+                end = mid;
+            }
+        }
+        
+        return nums[start];
+    }
+}
